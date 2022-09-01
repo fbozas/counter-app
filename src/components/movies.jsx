bbc8fed201596ce3e0fe33e66d12d8e17542a778
Panos Bozas <fbozas@hotmail.com>
2022-09-01 19:36:45 +0300

    Deleting a movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,14 @@ class Movies extends Component {
     state = {  
         movies: getMovies()
     } 
+
+    handleDelete = movie => {
+       console.log(this.state.movies);
+       let index = this.state.movies.indexOf(movie);
+       this.state.movies.splice(index, 1);
+       this.setState(this.state.movies);
+    }
+
     render() { 
         return (
             <table className='table'>
@@ -14,15 +22,23 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie => (
-                        <tr>
+                        <tr key={movie._id}>
                             <td>{movie.title}</td>
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <button 
+                                    onClick={() => this.handleDelete(movie)}
+                                    className='btn btn-danger btn-sm'>
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
