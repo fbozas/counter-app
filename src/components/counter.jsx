b053331290e6d4096affb8d3ae3bbda26f0625e8
Panos Bozas <fbozas@hotmail.com>
2022-09-01 18:52:53 +0300

    Update the state properly (working increment btn)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,12 +12,13 @@ class Counter extends Component {
     }
 
     handleIncrement = () => {
-        console.log("Increment Clicked!",this);
+        this.setState({count: this.state.count+1})
     }
 
     render() { 
         return (
            <div>
+                <span className={this.getBadgesClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
            </div>
         );
