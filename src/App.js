72317298019b680c9825a7cfb5cb63a3f54ea46b
Panos Bozas <fbozas@hotmail.com>
2022-09-02 18:59:21 +0300

    Http Services: Component preperation

--- src/App.js
+++ src/App.js
@@ -9,32 +9,59 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {  
-    count: 1
+    posts:[]
+  }
+
+  handleAdd = () => {
+    console.log("Add");
+  }
+
+  handleUpdate = post => {
+    console.log("Update", post);
+  }
+
+  handleDelete = post => {
+    console.log("Delete", post);
   }
   
-  componentDidUpdate(prevProps, prevState){
-    console.log("preveProps",prevProps);
-    console.log("prevState",prevState);
-    console.log(this.state.count);
-
-    // if(prevProps.state.count != this.props.state.count){
-    //   // ajax call to get new data from the server
-    // }
-  }
-
-  constructor(){
-    super();
-    console.log("App-Constructor");
-  }
-
-  componentDidMount(){
-    console.log("App-Mounted");
-    this.setState({count: this.state.count + 1})
-  }
-
   render() { 
-    console.log("App-Rendered");
-    return;
+    return (
+      <React.Fragment>
+          <button className='btn btn-primary' onClick={this.handleAdd}>
+            Add
+          </button>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.posts.map(post => (
+                <tr key={post.id}>
+                  <td>{post.title}</td>
+                  <td>
+                    <button 
+                      className='btn btn-info btn-sm' 
+                      onClick={() => this.handleUpdate(post)}>
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button 
+                      className='btn btn-danger btn-sm' 
+                      onClick={() => this.handleDelete(post)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </React.Fragment>
+    );
   }
 }
  
